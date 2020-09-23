b47eda42f55704c7e7389bd67cde1fc79d6bc11a
Theodore R. Smith <theodore@phpexperts.pro>
2020-09-23 14:59:35 -0500

    [x265] Pass all extra parameters directly to ffmpeg.

--- x265.sh
+++ x265.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-AUDIO=copy
-if [ ! -z "$2" ]; then
-    AUDIO="$2"
-fi
-
-CRF=27
-if [ ! -z "$3" ]; then
-    CRF="$3"
-fi
-
 mkdir -p x265
-time ffmpeg -y -hide_banner -hwaccel qsv -hwaccel_device /dev/dri/renderD128 -i "$1" -c:v libx265 -preset faster -x265-params crf=${CRF} -c:a ${AUDIO} "x265/$1.mkv"
+VIDEO=${1%.*}
+#time ffmpeg -y -hide_banner -hwaccel qsv -hwaccel_device /dev/dri/renderD128 -i "$1" -c:v libx265 -preset faster -x265-params crf=27 -c:a copy "x265/${VIDEO}.mkv"
+#time ffmpeg -y -hide_banner -hwaccel vaapi -vaapi_device /dev/dri/renderD128 -hwaccel_output_format vaapi -i "$1" -c:v hevc_qsv -preset slower -x265-params crf=27 -c:a copy 
+
+# For "${@:2}" @see https://stackoverflow.com/a/9057392/430062
+
+time LIBVA_DRIVER_NAME=iHD ffmpeg -y -hwaccel qsv -qsv_device /dev/dri/renderD128 -hwaccel_output_format qsv -i "$1" -c:v hevc_qsv -b:v 2500k -maxrate 6000k -bufsize 1200 -c:a copy "${@:2}" "x265/${VIDEO}.mkv" 
+if [ "$?" -eq 0 ]; then
+    mkdir -p x264
+    mv "$1" x264
+fi
+
