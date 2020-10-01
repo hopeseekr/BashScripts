777f90ef960bd02d64b2f2e1f6b9ad2e591e407a
Theodore R. Smith <theodore@phpexperts.pro>
2020-10-01 00:23:15 -0500

    [x265.sh] Use VAAPI for encoding using Intel graphics cards.

--- x265.sh
+++ x265.sh
@@ -7,7 +7,8 @@ VIDEO=${1%.*}
 
 # For "${@:2}" @see https://stackoverflow.com/a/9057392/430062
 
-time LIBVA_DRIVER_NAME=iHD ffmpeg -y -hwaccel qsv -qsv_device /dev/dri/renderD128 -hwaccel_output_format qsv -i "$1" -c:v hevc_qsv -b:v 2500k -maxrate 6000k -bufsize 1200 -c:a copy "${@:2}" "x265/${VIDEO}.mkv" 
+#time LIBVA_DRIVER_NAME=iHD ffmpeg -y -hwaccel qsv -qsv_device /dev/dri/renderD128 -hwaccel_output_format qsv -i "$1" -c:v hevc_qsv -b:v 2500k -maxrate 6000k -bufsize 1200 -c:a copy "${@:2}" "x265/${VIDEO}.mkv" 
+time LIBVA_DRIVER_NAME=iHD ffmpeg -y -hwaccel vaapi -vaapi_device /dev/dri/renderD128 -hwaccel_output_format vaapi -i "$1" -c:v hevc_vaapi -b:v 2500k -maxrate 6000k -bufsize 1200 -c:a copy "${@:2}" "x265/${VIDEO}.mkv" 
 if [ "$?" -eq 0 ]; then
     mkdir -p x264
     mv "$1" x264
